51d212f801b9c127e870a2fc136e7000ced87ba8
Tunuk Abdrakmanov <tunuk.abdrakmanov.pls@gmail.com>
2022-03-18 16:43:28 +0600

    shop_

--- main/views.py
+++ main/views.py
@@ -2,6 +2,9 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from django.views import View
+from django.views.generic import DetailView
+
 from .models import Category, Product
 
 
@@ -10,7 +13,17 @@ def index_page(request):
     return render(request, 'main/index.html', {'categories': categories})
 
 
+class ProductsListView(View):
+    def get(self, request):
+        products = Product.objects.all()
+        return render(request, 'main/products_list.html', {'products': products})
 
+
+class ProductDetailsView(DetailView):
+    queryset = Product.objects.all()
+    template_name = 'main/product_details.html'
+
+#TODO: сделать переходы между страницами
 #TODO: сделать список продуктов
 #TODO: авторизация
 #TODO: фильтрация, поиск, погинация
